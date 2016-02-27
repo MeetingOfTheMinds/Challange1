241137d1c8039e2ae8ec0d59377dc3d0bdd388dc
mxrss2 <mike.roth@mxrss.com>
2016-02-26 16:07:12 -0800

    Update to Project with solutions

--- Palindrome/Class1.cs
+++ Palindrome/Class1.cs
@@ -8,11 +8,56 @@ namespace Palindrome
     public class PalinDromeRunner
     {
 
-
-
-        public object IsPalindrome(char[] p)
+        public bool IsPalindrome(char[] phrase)
         {
-            return true;
+            bool result = false;
+            string stripedString = GetStrippedString(phrase);
+
+            string reversString = GetReverse(stripedString.ToCharArray());
+
+            for (int i = 0; i != stripedString.Length; i++)
+            {
+                if (stripedString[i] != reversString[i])
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    result = true;
+                }
+            }
+
+
+            return result;
         }
+
+        private string GetStrippedString(char[] phrase)
+        {
+            string resultingBuffer = string.Empty;
+
+            foreach (var letter in new String(phrase).ToLower())
+            {
+                if (letter >= 'a' && letter <= 'z')
+                {
+                    resultingBuffer += letter;
+                }
+            }
+
+            return resultingBuffer;
+        }
+
+        private string GetReverse(char[] phrase)
+        {
+            string result = string.Empty;
+
+            for (int i = phrase.Length - 1; i != -1; i--)
+            {
+                result += phrase[i];
+            }
+
+            return result;
+        }
+
     }
 }
